89ac63cd55f0fa582604f45fcf9c6b3318dc606b
Tadeo25 <juan_tadeo25@hotmail.com>
2021-05-24 20:12:22 -0300

    Listo el post de noticias

--- src/controllers/noticias.controllers.js
+++ src/controllers/noticias.controllers.js
@@ -6,5 +6,34 @@ noticiasCtrl.getPrueba = (req, res)=>{
     res.send("Hola desde el servidor");
   };
 
+  noticiasCtrl.noticiaNueva = async(req, res)=>{
+      try {
+          console.log(req.body.tituloNoticia);
+
+    //Creo noticia para guardar en BD
+          const nuevaNoticia = new NoticiaCollection({
+              tituloNoticia:req.body.tituloNoticia,
+              descripcionNoticia:req.body.descripcionNoticia,
+              cuerpoNoticia:req.body.cuerpoNoticia,
+              categoria:req.body.categoria
+          })
+
+    //Guardar objeto en BD
+            await nuevaNoticia.save();
+
+    //Enviar respuesta al frontend
+            res.status(201).json({
+                mensaje:"Noticia agregada correctamente 🤗"
+            });
+
+      } catch (error) {
+          console.log(error);
+          res.status(500).json({
+              mensaje:"Ocurrio un error al intentar agragar la noticia 😵"
+          })
+      }
+
+  };
+
   export default noticiasCtrl;
 
